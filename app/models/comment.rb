af5917da14249de0bcc59a33ff0f4b18126327d8
Alex Shereshovets <alexshereshovets@gmail.com>
2016-05-21 11:14:24 +0300

    completed iterations 2,3,4

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,4 +11,7 @@
 
 class Comment < ActiveRecord::Base
   belongs_to :post
+  belongs_to :user
+  validates_presence_of :title
+  validates_presence_of :body
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,8 @@
 #
 
 class Post < ActiveRecord::Base
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  validates_presence_of :title
+  validates_presence_of :body
 end
